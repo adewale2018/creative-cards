82846f919f3be54d8b2b5ac8e8e526f262b83a1f
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-05-31 01:08:21 +0100

    emit data to parent component, dynamic component, add additional card components

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-12">
-        <nav-header></nav-header>
-        <card-front></card-front>
+        <nav-header @pageWasChanged="currentPage=$event"></nav-header>
+        <component :is="currentPage"></component>
       </div>
     </div>
   </div>
@@ -11,12 +11,24 @@
 
 <script>
 import Header from "./components/Header.vue";
-import CardFront from './components/card/cardFront';
+import CardFront from "./components/card/cardFront.vue";
+import CardInsideLeft from "./components/card/cardInsideLeft.vue";
+import CardInsideRight from "./components/card/cardInsideRight.vue";
+import CardBack from "./components/card/cardBack";
+
 
 export default {
+  data: function() {
+    return {
+      currentPage: "cardFront"
+    };
+  },
   components: {
     navHeader: Header,
     cardFront: CardFront,
+    CardInsideLeft: CardInsideLeft,
+    CardInsideRight: CardInsideRight,
+    CardBack: CardBack,
   }
 };
 </script>
---
+++ src/components/card/cardInsideLeft.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Card Inside Left</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+  
+</style>
